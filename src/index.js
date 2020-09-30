028308abbcad629e7a8bc392c0360e8185fd959c
Mironov Vladimir <zidiks228@gmail.com>
2020-09-30 14:14:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof array !== 'undefined' && array.length > 0)
+        return Math.min.apply(null, array);
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array !== 'undefined' && array.length > 0)
+        return Math.max.apply(null, array);
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array !== 'undefined' && array.length > 0)
+        return array.reduce((sum, current) => sum + current) / array.length;
+    return 0;
 }
